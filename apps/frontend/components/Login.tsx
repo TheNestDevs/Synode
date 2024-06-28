76b0c3f8dfc7d0f33ceaae6b2eedd2efe7c98a20
DarkFalc0n <pratyayroy20@gmail.com>
2024-06-29 00:52:46 +0530

    fix: add webClientId to GoogleSignin

--- apps/frontend/components/Login.tsx
+++ apps/frontend/components/Login.tsx
@@ -9,37 +9,42 @@ import {
 } from '@react-native-google-signin/google-signin';
 
 export default function Login(): React.JSX.Element {
-    const [userState, setUserState] = React.useState({
-        userInfo: undefined as User | undefined,
-        error: undefined,
-      });
+  const [userState, setUserState] = React.useState({
+    userInfo: undefined as User | undefined,
+    error: undefined,
+  });
 
-      const onGoogleSignIn = async () => {
-        try {
-            await GoogleSignin.configure();
-            await GoogleSignin.hasPlayServices();
-            const userInfo = await GoogleSignin.signIn();
-            setUserState({ userInfo, error: undefined });
-          } catch (error) {
-            if (isErrorWithCode(error)) {
-              switch (error.code) {
-                case statusCodes.SIGN_IN_CANCELLED:
-                  console.log('User cancelled the sign in flow');
-                  break;
-                case statusCodes.IN_PROGRESS:
-                    console.log('Sign in is in progress');
-                  break;
-                case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
-                    console.log('Play Services not available');
-                  break;
-                default:
-                    console.log('Something went wrong', error);
-              }
-            } else {
-              console.log('Error', error);
-            }
-          }
+  const onGoogleSignIn = async () => {
+    try {
+      await GoogleSignin.configure({
+        webClientId:
+        '716159749536-n3s97ghkaqggaklrsfbe4mf2s103runo.apps.googleusercontent.com',
+        offlineAccess: true,
+      });
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      console.log('token', userInfo.idToken);
+      setUserState({userInfo, error: undefined});
+    } catch (error) {
+      if (isErrorWithCode(error)) {
+        switch (error.code) {
+          case statusCodes.SIGN_IN_CANCELLED:
+            console.log('User cancelled the sign in flow');
+            break;
+          case statusCodes.IN_PROGRESS:
+            console.log('Sign in is in progress');
+            break;
+          case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+            console.log('Play Services not available');
+            break;
+          default:
+            console.log('Something went wrong', error);
+        }
+      } else {
+        console.log('Error', error);
+      }
     }
+  };
 
   return (
     <SafeAreaView>
