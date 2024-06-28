aa5a948f39e5ec68f1ede19edb5c909e422060a0
DarkFalc0n <pratyayroy20@gmail.com>
2024-06-28 23:58:14 +0530

    feat: add Google SignIn

---
+++ apps/frontend/.d.ts
@@ -0,0 +1,1 @@
+declare module '@gustash/react-native-smart-lock';
---
+++ apps/frontend/components/Login.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {SafeAreaView, Text, View, Pressable} from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  isErrorWithCode,
+  statusCodes,
+  User,
+} from '@react-native-google-signin/google-signin';
+
+export default function Login(): React.JSX.Element {
+    const [userState, setUserState] = React.useState({
+        userInfo: undefined as User | undefined,
+        error: undefined,
+      });
+
+      const onGoogleSignIn = async () => {
+        try {
+            await GoogleSignin.configure();
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            setUserState({ userInfo, error: undefined });
+          } catch (error) {
+            if (isErrorWithCode(error)) {
+              switch (error.code) {
+                case statusCodes.SIGN_IN_CANCELLED:
+                  console.log('User cancelled the sign in flow');
+                  break;
+                case statusCodes.IN_PROGRESS:
+                    console.log('Sign in is in progress');
+                  break;
+                case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+                    console.log('Play Services not available');
+                  break;
+                default:
+                    console.log('Something went wrong', error);
+              }
+            } else {
+              console.log('Error', error);
+            }
+          }
+    }
+
+  return (
+    <SafeAreaView>
+      <GoogleSigninButton onPress={onGoogleSignIn} />
+      <Text>
+        {userState.userInfo
+          ? `Hello, ${userState.userInfo.user.name}!`
+          : 'Please sign in'}
+      </Text>
+    </SafeAreaView>
+  );
+}
