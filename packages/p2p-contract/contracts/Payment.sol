7c118f26681b1358081a6939b13bd5f849bc88dd
ATLAS2002 <siddharth211102@gmail.com>
2024-06-29 17:44:14 +0530

    fix: add store in contract

--- packages/p2p-contract/contracts/Payment.sol
+++ packages/p2p-contract/contracts/Payment.sol
@@ -1,67 +1,48 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-// import "@thirdweb-dev/contracts/base/ERC20Base.sol";
-// // import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@thirdweb-dev/contracts/base/ERC20Base.sol";
 
-// contract Payment is ERC20Base {
-//     struct PaymentInfo {
-//         address from;
-//         address to;
-//         uint256 amount;
-//         uint256 timestamp;
-//     }
+contract Payment is ERC20Base {
+    struct TransactionInfo {
+        address sender;
+        address reciever;
+        uint256 amount;
+        uint256 timestamp;
+    }
 
-//     PaymentInfo[] private _payments;
-//     mapping(address => PaymentInfo[]) private _paymentOf;
+    mapping (uint256=>TransactionInfo) private _payment;
 
-//     constructor()
-//         ERC20Base(
-//             _defaultAdmin,
-//             _name,
-//             _symbol
-//         )
-//     {
-//         _defaultAdmin = _defaultAdmin;
-//         _name = _name;
-//         _symbol = _symbol;
-//     }
+    constructor(
+        address _defaultAdmin,
+        string memory _name,
+        string memory _symbol
+    )
+        ERC20Base(
+            _defaultAdmin,
+            _name,
+            _symbol
+        )
+    {
+        _defaultAdmin = _defaultAdmin;
+        _name = _name;
+        _symbol = _symbol;
+    }
 
-//     // function _transfer(address payable _from, address payable _to, uint256 _amount) internal override {
-//     //     require(_from != address(0), "Payment: transfer from the zero address");
-//     //     require(_to != address(0), "Payment: transfer to the zero address");
-//     //     require(_amount > 0, "Payment: transfer amount must be greater than zero");
-//     //     require(balanceOf(_from) >= _amount, "Payment: transfer amount exceeds balance");
+    // function sendTokens(address payable _from, address payable _to) public payable {
+    //     _transfer(_from, _to, msg.value);
+    // }
+    function storeTransaction(address from, address to, uint256 amount, uint256 timestamp) internal {
+        _payment[timestamp] = TransactionInfo(from, to, amount, timestamp);
+    }
 
-//     //     PaymentInfo memory pmt = PaymentInfo({
-//     //         from: _from,
-//     //         to: _to,
-//     //         amount: _amount,
-//     //         timestamp: block.timestamp
-//     //     });
+    function mintTokens(address payable _address) public payable {
+        _address.transfer(msg.value);
+        storeTransaction(msg.sender, _address, msg.value, block.timestamp);
+        emit Transfer(msg.sender, _address, msg.value);
+    }
 
-//     //     _paymentOf[_from].push(pmt);
-//     //     _paymentOf[_to].push(pmt);
-//     //     _payments.push(pmt);
-//     // }
-
-//     function sendTokens(address payable _from, address payable _to) public payable {
-//         _transfer(_from, _to, msg.value);
-//     }
-
-//     function mintTokens(address payable _address) public payable {
-//         _address.transfer(msg.value);
-//     }
-
-//     function getPayments(address _address) public view returns (PaymentInfo[] memory) {
-//         return _paymentOf[_address];
-//     }
-
-//     function getBalance(address _address) public view returns (uint256) {
-//         return balanceOf(_address);
-//     }
-
-//     function showHistory() public view returns (PaymentInfo[] memory) {
-//         return _paymentOf[msg.sender];
-//     }
-// }
+    function getTransaction(uint256 snowflake) public view returns (TransactionInfo memory) {
+        return _payment[snowflake];
+    }
+}
